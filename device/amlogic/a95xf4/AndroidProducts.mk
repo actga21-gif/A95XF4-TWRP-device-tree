215e3baa5f462c8c819986fcde5ebbad278120e8
actga21-gif <actga21@gmail.com>
2026-09-03 09:29:02 +0530

    Add files via upload

---
+++ device/amlogic/a95xf4/AndroidProducts.mk
@@ -0,0 +1,5 @@
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_a95xf4.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_a95xf4-eng
---
+++ device/amlogic/a95xf4/BoardConfig.mk
@@ -0,0 +1,41 @@
+DEVICE_PATH := device/amlogic/a95xf4
+
+TARGET_ARCH := arm
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_VARIANT := cortex-a55
+
+TARGET_BOARD_PLATFORM := sc2
+TARGET_BOOTLOADER_BOARD_NAME := ohm
+
+# A95X F4 stock boot image is Android boot image header v3.
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_IMAGE_NAME := kernel
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/kernel
+
+# This device boots recovery from the boot partition; there is no separate
+# recovery_a/recovery_b partition in the supplied stock partition set.
+BOARD_USES_RECOVERY_AS_BOOT := true
+
+# Dynamic partitions / A-B / virtual A-B.
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+AB_OTA_UPDATER := true
+
+# TWRP UI defaults for the Android TV box.
+TW_THEME := portrait_hdpi
+TW_SCREEN_WIDTH := 1920
+TW_SCREEN_HEIGHT := 1080
+TW_INCLUDE_FASTBOOTD := true
+TW_INCLUDE_REPACKTOOLS := true
+TW_USE_TOOLBOX := true
+TW_NO_REBOOT_BOOTLOADER := true
+
+# Encryption / Android 11.
+TW_INCLUDE_CRYPTO := true
+TW_INCLUDE_FBE_METADATA_DECRYPT := true
+TW_USE_FSCRYPT_POLICY_V1 := false
+
+# The target is 32-bit userspace.
+TW_EXTRA_LANGUAGES := false
